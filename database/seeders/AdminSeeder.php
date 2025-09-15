1c122acd617a3e2f8a186a7ef251a99157e2d102
MuhammedKkhaled <mohamedkhaled25179@gmail.com>
2025-09-15 21:49:08 +0300

    Replace server.php with index.php for handling requests in Laravel application

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            [
+                "email" => "admin@sensing-nature.com"
+            ],
+            [
+                "name" => "Admin",
+                "password" => bcrypt("password"),
+                "is_admin" => true,
+            ]
+        );
+    }
+}
